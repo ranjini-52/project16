ef612dcaa5aae6602de078132ab9a4172f6b3414
Your Nameranjini <ranjini782000@gmail.com>
2023-04-11 16:40:09 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sam1(request):
+    return render(request,'sam1.html')
+def sam1login(request):
+    return render(request,'sam1login.html')
